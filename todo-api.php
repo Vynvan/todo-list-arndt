9dee31519e0269f64784223cd0e8bd2a8522b500
vynvan_server <giant_innovations@live.de>
2024-09-27 14:53:21 +0200

    Added logging

--- todo-api.php
+++ todo-api.php
@@ -1,17 +1,28 @@
 <?php
 header("Content-Type: application/json");
 
+function write_log($action, $data) {
+    $log = fopen('log.txt', 'a');
+    $timestamp = date('Y-m-d H:i:s');
+    fwrite($log, "$timestamp - $action: " . json_encode($data) . "\n");
+    fclose($log);
+}
+
 switch($_SERVER['REQUEST_METHOD']) {
     case 'GET':
         // Get Todos (READ)
+        write_log("READ", null);
         break;
     case 'POST':
         // Add todo (CREATE)
+        write_log("CREATE", null);
         break;
     case 'PUT':
         // Change todo (UPDATE)
+        write_log("UPDATE", null);
         break;
     case 'DELETE':
         // Remove todo (DELETE)
+        write_log("DELETE", null);
         break;
 }
