fa9faf039d28f2ccffa00fab89ab998ed5b6c8b4
vynvan_server <giant_innovations@live.de>
2024-09-30 10:03:23 +0200

    Added log.txt and fetch for get

--- todo-list.js
+++ todo-list.js
@@ -2,5 +2,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Define the URL to our PHP API
     const apiUrl = "todo-api.php";
-    
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        const todoList = document.getElementById('todo-list');
+        data.forEach(item => {
+            const li = document.createElement('li');
+            li.textContent = item.title;
+            todoList.appendChild(li);
+        });
+    });
+
 });
