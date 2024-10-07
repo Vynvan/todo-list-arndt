f4649f3fa4f2f60ba75842ed01e849392fb63953
vynvan_server <giant_innovations@live.de>
2024-10-07 14:07:51 +0200

    Implemented todo-db-api.php and updated todo-api to use mariaDb

--- todo-api.php
+++ todo-api.php
@@ -1,9 +1,9 @@
 <?php
+require_once('todo-db-api.php');
 header("Content-Type: application/json");
 
 $todo_file = 'todos.json';
-$todo_items = json_decode(file_get_contents($todo_file), true);
-usort($todo_items, "sortByIx");
+$todo_items = getAll();
 
 switch($_SERVER['REQUEST_METHOD']) {
     case 'GET':
@@ -16,62 +16,26 @@ switch($_SERVER['REQUEST_METHOD']) {
         $data = json_decode(file_get_contents('php://input'), true);
         $ix = (end($todo_items)['ix'] ?? -1) + 1;
         $new_todo = ["id" => uniqid(), "title" => $data['title'], "ix" => $ix, "userid" => 1];
-        $todo_items[] = $new_todo;
-        file_put_contents($todo_file, json_encode($todo_items));
+        $new_todo['result'] = insertTodo($new_todo);
         echo json_encode($new_todo);
         write_log("CREATE", $new_todo);
         break;
     case 'PUT':
         // Change todo (UPDATE):
         $data = json_decode(file_get_contents('php://input'), true);
-        $toUpdate = $data['id'];
-        $todo_id = getById($todo_items, $toUpdate);
-        if ($todo_id !== null) {
-            if (isset($data['title'])) {
-                $todo_items[$todo_id]['title'] = $data['title'];
-            }
-            if (isset($data['done'])) {
-                if ($data['done'] === true) $todo_items[$todo_id]['done'] = true;
-                else unset($todo_items[$todo_id]['done']);
-                // $todo_items[$todo_id]['done'] = $data['done'];
-            }
-            if (isset($data['ix'])) {
-                $todo_items[$todo_id]['ix'] = $data['ix'];
-            }
-            file_put_contents($todo_file, json_encode($todo_items));
-            echo json_encode($data);
-            write_log("UPDATE", $data);
-        }
-        else echo json_encode(array("error" => "Fehler: Todo nicht gefunden $toUpdate => $todo_id"));
+        $data['result'] = updateTodo($data);
+        echo json_encode($data);
+        write_log("UPDATE", $data);
         break;
     case 'DELETE':
         // Remove todo (DELETE):
         $data = json_decode(file_get_contents('php://input'), true);
-        $toUpdate = $data['id'];
-        $todo_id = getById($todo_items, $toUpdate);
-        if ($todo_id != null) {
-            unset($todo_items[$todo_id]);
-            file_put_contents($todo_file, json_encode($todo_items));
-            echo json_encode($data);
-            write_log("DELETE", $data);
-        }
-        else echo json_encode(array("error" => "Fehler: Todo nicht gefunden $toUpdate => $todo_id"));
+        $data['result'] = deleteTodo($data);
+        echo json_encode($data);
+        write_log("DELETE", $data);
         break;
 }
 
-
-function getById($array, $id) {
-    foreach ($array as $key => $val) {
-        if ($val['id'] === $id) {
-            return $key;
-        }
-    }
-    return null;
-}
-
-function sortByIx($item1, $item2) {
-    return $item1['ix'] - $item2['ix'];
-}
 
 function write_log($action, $data) {
     $log = fopen('log.txt', 'a');
---
+++ todo-db-api.php
@@ -0,0 +1,58 @@
+<?php
+ 
+function getPDO () {
+    try {
+        $user = 'todo-user';
+        $pass = '0677';
+        $host = '127.0.0.1';
+        $db = 'todolist';
+        $charset = 'utf8mb4'; 
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]; 
+        
+                $pdo = new PDO($dsn, $user, $pass, $options);
+        return $pdo;
+    } catch (PDOException $e) {
+        error_log("PDOException: " . $e->getMessage() . " in " . $e->getFile() . " on line " . $e->getLine());
+        return null;
+    }
+}
+
+function getAll() {
+    $pdo = getPDO();
+    $stmt = $pdo->query("SELECT id, text AS title, completed AS done, ix FROM todos WHERE userid=1 ORDER BY ix");
+    $items = $stmt->fetchAll();
+    return $items;
+}
+
+function insertTodo($item) {
+    $pdo = getPDO();
+    $stmt = $pdo->prepare("INSERT INTO todos (text, ix, userid) VALUES (:text, :ix, :userid)");
+    $result = $stmt->execute(["text" => $item['title'], "ix" => $item['ix'], "userid" => 1]);
+    return $result;
+}
+
+function deleteTodo($item) {
+    $pdo = getPDO();
+    $stmt = $pdo->prepare("DELETE FROM todos WHERE id=:id");
+    $result = $stmt->execute(["id" => $item['id']]);
+    return $result;
+}
+
+function updateTodo($item) {
+    $uDone = isset($item['done']);
+    $uIx = isset($item['done']);
+    $uText = isset($item['title']);
+    if ($uDone || $uIx || $uText) {
+        $done = $uDone ? " completed=:done" : "";
+        $ix = $uIx ? " ix=:ix" : "";
+        $text = $uText ? " text=:text " : "";
+        $pdo = getPDO();
+        $stmt = $pdo->prepare("UPDATE todos SET" . $done . ($done && $uIx ? ", " : "") . $ix . (($done || $uIx) && $text ? ", " : "") . $text . "WHERE id=:id");
+        $result = $stmt->execute(["id" => $item['id'], "text" => $item['title'], "ix" => $item['ix'], "done" => ($item['done'] ? 1 : 0)]);
+        return $result;
+    }
+}
