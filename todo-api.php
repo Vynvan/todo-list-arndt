d273a58e7a817532994d902e3f93bf4214eec7ca
vynvan_server <giant_innovations@live.de>
2024-10-08 09:39:53 +0200

    Added config.php

--- todo-api.php
+++ todo-api.php
@@ -1,5 +1,5 @@
 <?php
-require_once('todo-db-api.php');
+require_once 'todo-db-api.php';
 header("Content-Type: application/json");
 
 $todo_file = 'todos.json';
--- todo-db-api.php
+++ todo-db-api.php
@@ -1,19 +1,14 @@
 <?php
- 
-function getPDO () {
+
+function getPDO() {
     try {
-        $user = 'todo-user';
-        $pass = '0677';
-        $host = '127.0.0.1';
-        $db = 'todolist';
-        $charset = 'utf8mb4'; 
+        require_once 'config.php';
         $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
         $options = [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ]; 
-        
-                $pdo = new PDO($dsn, $user, $pass, $options);
+        ];
+        $pdo = new PDO($dsn, $user, $pass, $options);
         return $pdo;
     } catch (PDOException $e) {
         error_log("PDOException: " . $e->getMessage() . " in " . $e->getFile() . " on line " . $e->getLine());
