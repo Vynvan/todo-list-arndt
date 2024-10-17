735fc6a6f9fbc6237f17022a5a8a4172c1005e6f
vynvan_server <giant_innovations@live.de>
2024-10-17 15:02:21 +0200

    Added comments

--- todo-list.js
+++ todo-list.js
@@ -33,13 +33,21 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('abort-edit').addEventListener('click', () => switchForms());
 });
 
-// Calls createTodo for the given item and adds the returned li to the given list
+/**
+ * Calls createTodo for the given item and adds the returned li to the given list.
+ * @param {*} list 
+ * @param {*} item 
+ */
 function addTodoElement(list, item) {
     const li = createTodoElement(item);
     list.appendChild(li);
 }
 
-// Adds the delete button with funtionality for the given item to the given li-element
+/**
+ * Adds the delete button with funtionality for the given item to the given li-element.
+ * @param {*} li 
+ * @param {*} item 
+ */
 function addDeleteButton(li, item) {
     const button = createRoundButton();
     button.classList.add('del-btn');
@@ -61,7 +69,11 @@ function addDeleteButton(li, item) {
     li.appendChild(button);
 }
 
-// Adds the done button with funtionality for the given item to the given li-element
+/**
+ * Adds the done button with funtionality for the given item to the given li-element.
+ * @param {*} li 
+ * @param {*} item 
+ */
 function addDoneButton(li, item) {
     const button = createRoundButton();
     button.classList.add('done-btn');
@@ -99,11 +111,10 @@ function addEditButton(li, item) {
     const button = createRoundButton();
     button.classList.add('edit-btn');
     button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16"><path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/></svg>';
-    button.addEventListener('click', () => {
+    button.addEventListener('click', (ev) => {
         const editForm = document.getElementById('edit-form');
         const editInput = document.getElementById('edit-input');
         if (editForm.classList.contains('hidden')) {
-            editInput.value = item.title;
             editForm.removeEventListener('submit', editListener);
             editListener = (ev) => {
                 ev.preventDefault();
@@ -113,6 +124,7 @@ function addEditButton(li, item) {
             editForm.addEventListener('submit', editListener);
         }
         switchForms();
+        editInput.value = item.title;
     });
     li.appendChild(button);
 }
